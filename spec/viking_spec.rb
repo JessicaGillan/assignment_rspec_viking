8c3cf1927e5a10f6593730f29202db2282c500d0
Jessica G <gillan.jessica@gmail.com>
2016-11-04 13:01:18 -0600

    almost finish viking tests

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -4,6 +4,7 @@ require 'weapons/weapon'
 describe Viking do
   before { allow($stdout).to receive(:puts) }
   let(:viking){ Viking.new("Oleg", 70) }
+  let(:other_viking){ Viking.new("Hilda", 80) }
   let(:cupcake_weapon){ Weapon.new("Cupcake") }
   let(:stapler_weapon){ Weapon.new("Stapler") }
 
@@ -48,6 +49,87 @@ describe Viking do
       viking.pick_up_weapon(stapler_weapon)
       expect(viking.weapon).to eq(stapler_weapon)
     end
+  end
 
+  describe '#drop_weapon' do
+  	it 'leaves the viking weaponless' do
+  		viking.pick_up_weapon(cupcake_weapon)
+  		viking.drop_weapon
+  		expect(viking.weapon).to be_nil
+  	end
+  end
+
+  describe '#receive_attack' do
+  	it 'reduces the viking\'s health by the specified amount' do
+  		viking.receive_attack(69)
+  		expect(viking.health).to eq(1)
+  	end
+
+  	it 'calls the take_damage method' do
+  		expect(viking).to receive(:take_damage)
+  		viking.receive_attack(69)
+  	end
+  end
+
+  describe '#attack' do
+  	it 'damages the health of the viking attacked' do
+  		viking.attack(other_viking)
+  		expect(other_viking.health).to be < 80
+  	end
+
+  	it 'calls the attacked viking\'s take_damage method' do
+  		expect(other_viking).to receive(:take_damage)
+  		viking.attack(other_viking)
+  	end
+
+  	it 'runs damage_with_fists if viking has no weapon' do
+  		expect(viking).to receive(:damage_with_fists).and_return(2.5)
+
+  		viking.attack(other_viking)
+  	end
+
+  	it 'deals fist multiplier times strength damage when no weapon' do
+  		expect(other_viking).to receive(:receive_attack).with(2.5)
+
+  		viking.attack(other_viking)
+  	end
+
+  	it 'runs damage_with_weapon if viking has weapon' do
+  		expect(viking).to receive(:damage_with_weapon).and_return(2.5)
+
+  		viking.pick_up_weapon(cupcake_weapon)
+  		viking.attack(other_viking)
+  	end
+
+  	it 'deals weapon multiplier times strength damage when have weapon' do
+  		expect(other_viking).to receive(:receive_attack).with(10)
+
+  		viking.pick_up_weapon(cupcake_weapon)
+  		viking.attack(other_viking)
+  	end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
