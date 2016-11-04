42c9a093ed958827f444913dc6fe804d5c8e681e
Jessica G <gillan.jessica@gmail.com>
2016-11-04 12:08:12 -0600

    tests for bow and viking

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,21 @@
-# Your code here
+require 'viking'
+
+describe Viking do
+  before { allow($stdout).to receive(:puts) }
+
+	describe '#initialize' do
+		let(:viking){ Viking.new("Oleg", 70) }
+
+		it 'sets a passed name to the name attribute' do
+			expect(viking.name).to eq("Oleg")
+		end
+
+		it 'sets a passed health to the health attribute' do
+			expect(viking.health).to eq(70)
+		end
+
+		it 'does not allow health to be overwritten after initialize' do
+			expect{ viking.health = 90 }.to raise_error(NoMethodError)
+		end
+	end
+end
---
+++ spec/weapons/bow_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../../lib/weapons/bow'
+
+describe Bow do
+	let(:bow){ Bow.new }
+
+	describe '#arrows' do
+		it 'returns arrow count' do
+			expect(Bow.new(20).arrows).to eq(20)
+		end
+
+		it 'has a default start of ten arrows' do
+			expect(bow.arrows).to eq(10)
+		end
+
+		it 'can be set with a specified amount of arrows' do
+			expect(Bow.new(20).arrows).to eq(20)
+		end
+	end
+
+	describe '#use' do
+		it "reduces number of arrows by one" do
+			bow.use
+			expect(bow.arrows).to eq(9)
+		end
+
+		it 'throws an error if it has 0 arrows' do
+			bow = Bow.new(0)
+			expect{ bow.use }.to raise_error("Out of arrows")
+		end
+	end
+end
